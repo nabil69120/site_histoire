9ca47c7ffa1bc0273294b21597cb85ab877678df
nabil69120 <nabilaarablyon@gmail.com>
2022-06-16 19:20:44 +0200

    hgjk

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
     include("config/config.php");
 
     $page_controller = 'c_index.php';
@@ -8,13 +8,5 @@
         $page_controller = "c_".$_GET['page'].'.php';
     }
 
-    include("controller/".$page_controller);*/
+    include("controller/".$page_controller);
 ?>
-<?php echo "hako le + gros"; ?>
-<html>
-    <head>
-    </head>
-    <body>
-        <h1>test</h1>
-    </body>
-</html>
